30a9797e740566ba343f9ed52cf94e2772f3c830
masazdream <masaz.dream@gmail.com>
2016-12-05 10:24:57 +0900

    add japanese.

--- main.py
+++ main.py
@@ -4,37 +4,37 @@ from PIL import Image, ImageDraw, ImageFont
 import os.path
 
 # label setting for 20
-Label = namedtuple('Label', ['name','id','trainId','category','categoryId','hasInstances','ignoreInEval','color'])
+Label = namedtuple('Label', ['name', 'jp', 'id','trainId','category','categoryId','hasInstances','ignoreInEval','color'])
 
-font_path = '/System/Library/Fonts/ヒラギノ明朝 ProN W3.otf'
+font_path = '/System/Library/Fonts/ヒラギノ角ゴ ProN W3.otf'
 
 labellist = [
-    #       name                     id    trainId   category            catId     hasInstances   ignoreInEval   color
-    Label(  'road'                 ,  7 ,        0 , 'flat'            , 1       , False        , False        , (128, 64,128) ),
-    Label(  'sidewalk'             ,  8 ,        1 , 'flat'            , 1       , False        , False        , (244, 35,232) ),
-    Label(  'building'             , 11 ,        2 , 'construction'    , 2       , False        , False        , ( 70, 70, 70) ),
-    Label(  'guard rail'           , 14 ,        3 , 'construction'    , 2       , False        , True         , (180,165,180) ),
-    Label(  'fence'                , 13 ,        4 , 'construction'    , 2       , False        , False        , (190,153,153) ),
-    Label(  'pole'                 , 17 ,        5 , 'object'          , 3       , False        , False        , (153,153,153) ),
-    Label(  'traffic light'        , 19 ,        6 , 'object'          , 3       , False        , False        , (250,170, 30) ),
-    Label(  'traffic sign'         , 20 ,        7 , 'object'          , 3       , False        , False        , (220,220,  0) ),
-    Label(  'vegetation'           , 21 ,        8 , 'nature'          , 4       , False        , False        , (107,142, 35) ),
-    Label(  'terrain'              , 22 ,        9 , 'nature'          , 4       , False        , False        , (152,251,152) ),
-    Label(  'sky'                  , 23 ,       10 , 'sky'             , 5       , False        , False        , ( 70,130,180) ),
-    Label(  'person'               , 24 ,       11 , 'human'           , 6       , True         , False        , (220, 20, 60) ),
-    Label(  'unlabeled'            ,  0 ,       12 , 'void'            , 0       , False        , True         , (  0,  0,  0) ),
-    Label(  'car'                  , 26 ,       13 , 'vehicle'         , 7       , True         , False        , (  0,  0,142) ),
-    Label(  'truck'                , 27 ,       14 , 'vehicle'         , 7       , True         , False        , (  0,  0, 70) ),
-    Label(  'bus'                  , 28 ,       15 , 'vehicle'         , 7       , True         , False        , (  0, 60,100) ),
-    Label(  'train'                , 31 ,       16 , 'vehicle'         , 7       , True         , False        , (  0, 80,100) ),
-    Label(  'motorcycle'           , 32 ,       17 , 'vehicle'         , 7       , True         , False        , (  0,  0,230) ),
-    Label(  'bicycle'              , 33 ,       18 , 'vehicle'         , 7       , True         , False        , (119, 11, 32) ),
-    Label(  'lanemkgsdriv'         , 34 ,       19 , 'flat'            , 1       , False        , True         , (128,  0,192) ),
+    #       name                          id    trainId   category            catId     hasInstances   ignoreInEval   color
+    Label(  'road'                 , u'道路',  7 ,        0 , 'flat'            , 1       , False        , False        , (128, 64,128) ),
+    Label(  'sidewalk'             , u'歩道',  8 ,        1 , 'flat'            , 1       , False        , False        , (244, 35,232) ),
+    Label(  'building'             , u'建物',  11 ,        2 , 'construction'    , 2       , False        , False        , ( 70, 70, 70) ),
+    Label(  'guard rail'           , u'ガードレール',  14 ,        3 , 'construction'    , 2       , False        , True         , (180,165,180) ),
+    Label(  'fence'                , u'フェンス',  13 ,        4 , 'construction'    , 2       , False        , False        , (190,153,153) ),
+    Label(  'pole'                 , u'ポール',  17 ,        5 , 'object'          , 3       , False        , False        , (153,153,153) ),
+    Label(  'traffic light'        , u'信号',  19 ,        6 , 'object'          , 3       , False        , False        , (250,170, 30) ),
+    Label(  'traffic sign'         , u'標識',  20 ,        7 , 'object'          , 3       , False        , False        , (220,220,  0) ),
+    Label(  'vegetation'           , u'草や木など',  21 ,        8 , 'nature'          , 4       , False        , False        , (107,142, 35) ),
+    Label(  'terrain'              , u'自然な地面',  22 ,        9 , 'nature'          , 4       , False        , False        , (152,251,152) ),
+    Label(  'sky'                  , u'空',  23 ,       10 , 'sky'             , 5       , False        , False        , ( 70,130,180) ),
+    Label(  'person'               , u'人',  24 ,       11 , 'human'           , 6       , True         , False        , (220, 20, 60) ),
+    Label(  'unlabeled'            , u'ラベル無し',   0 ,       12 , 'void'            , 0       , False        , True         , (  0,  0,  0) ),
+    Label(  'car'                  , u'車',  26 ,       13 , 'vehicle'         , 7       , True         , False        , (  0,  0,142) ),
+    Label(  'truck'                , u'トラック',  27 ,       14 , 'vehicle'         , 7       , True         , False        , (  0,  0, 70) ),
+    Label(  'bus'                  , u'バス',  28 ,       15 , 'vehicle'         , 7       , True         , False        , (  0, 60,100) ),
+    Label(  'train'                , u'電車',  31 ,       16 , 'vehicle'         , 7       , True         , False        , (  0, 80,100) ),
+    Label(  'motorcycle'           , u'バイク',  32 ,       17 , 'vehicle'         , 7       , True         , False        , (  0,  0,230) ),
+    Label(  'bicycle'              , u'自転車',  33 ,       18 , 'vehicle'         , 7       , True         , False        , (119, 11, 32) ),
+    Label(  'lanemkgsdriv'         , u'白線',  34 ,       19 , 'flat'            , 1       , False        , True         , (128,  0,192) ),
 ]
 
 def main():
     print("class number: %d" % len(labellist))
-    font = ImageFont.truetype(font_path, 20, encoding="UTF=8")
+    font = ImageFont.truetype(font_path, 20, encoding='UTF-8')
 
     img = Image.new("RGB", (800, 150), (255, 255, 255))
     draw = ImageDraw.Draw(img)
@@ -42,7 +42,7 @@ def main():
         raw = l / 4
         colum = l % 4
         color = labellist[l].color
-        name = labellist[l].name
+        name = labellist[l].jp
 
         print raw
         print colum
